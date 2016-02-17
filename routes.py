3a8e7da1c2fbf3aafd0ce349c14d4d12ee157676
Uma Petersen <Uma@Umas-MacBook-Pro.local>
2016-02-17 10:26:38 -0800

    added blue color to text and fixed layout

---
+++ routes.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from flask import Flask, render_template
+
+
+
+################################################################################
+app = Flask(__name__)
+################################################################################
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "freedom")
+################################################################################
+                             ## ROUTES ##
+################################################################################
+
+@app.route("/")
+def index():
+    """ my about me page"""
+
+  
+
+    return render_template("index.html")
+
+
+
+
+################################################################################
+if __name__ == "__main__":
+    
+    PORT = int(os.environ.get("PORT", 5000))
+    DEBUG = "NO_DEBUG" not in os.environ
+
+    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
